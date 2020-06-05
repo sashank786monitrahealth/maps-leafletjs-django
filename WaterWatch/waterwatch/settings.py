a01732b863df20f8136875542fb159b1530a2d6f
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-06-05 14:45:55 +0530

    update README.md

--- WaterWatch/waterwatch/settings.py
+++ WaterWatch/waterwatch/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
+    'waterwatchapp'
 ]
 
 MIDDLEWARE = [
@@ -73,10 +75,20 @@ WSGI_APPLICATION = 'waterwatch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'cptwater',
+        'USER':'waterwatch',
+        'PASSWORD':'postgres',
+        'HOST':'localhost',
     }
 }
 
